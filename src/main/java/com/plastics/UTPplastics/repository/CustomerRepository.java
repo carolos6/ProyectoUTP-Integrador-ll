a303428c2dddbeb798922f2a9be31ac29de3bae3
carolos6 <carolos_kk_xx@hotmail.com>
2024-09-19 13:07:08 -0500

    Se agrego una nuevO @Query findByEmailAndPassword

--- src/main/java/com/plastics/UTPplastics/repository/CustomerRepository.java
+++ src/main/java/com/plastics/UTPplastics/repository/CustomerRepository.java
@@ -12,6 +12,9 @@ public interface CustomerRepository extends CrudRepository<Customer, Integer> {
     @Query("SELECT c FROM Customer c WHERE c.email LIKE %:email% OR c.address LIKE %:address%")
     List<Customer> findByEmailOrAddress(@Param("email") String email,@Param("address") String address);
 
+    @Query("SELECT c FROM Customer c WHERE c.email = :email AND  c.password = :password")
+    List<Customer> findByEmailAndPassword(@Param("email") String email,
+                                          @Param("password") String password);
 
 
 
